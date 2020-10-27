51268c3c99128320d1a947c3eb7723f5ed0789af
evachengrq <evachengrq@hotmail.com>
2020-10-27 21:45:45 +0800

    [evacheng]feat: finished the display style of student list component

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import './StudentList.css';
 
 class StudentList extends Component {
+  state = ["成吉思汗", "鲁班七号", "太乙真人", "钟无艳", "花木兰", "雅典娜", "芈月", "白起", "刘禅", "庄周", "马超", "刘备", "哪吒", "大乔", "蔡文姬"]
+
   render() {
     return (
       <section className="studentList">
           <h1>学员列表</h1>
-          <button>+ 添加学员</button>
+          <section>
+            {this.state.map(item => (
+              <button className="studentLabels">{item}</button>
+            ))}
+            <button className="addStudent">+ 添加学员</button>
+          </section>
       </section>
     );
   }
